440d662958d932b816eaeafbe1c99978a95a4c57
Momo-shz <songhongze94@sina.com>
2023-11-06 23:04:23 +0800

    add project

---
+++ cubeDemo/Core/Inc/LED.h
@@ -0,0 +1,9 @@
+#ifndef LED_H
+#define LED_H
+
+#include "stm32f1xx_hal.h"
+extern void LED_Mainfunction_1_20ms(void);
+
+extern void LED_Mainfunction_2_20ms(void);
+
+#endif
---
+++ cubeDemo/Core/Src/LED.c
@@ -0,0 +1,23 @@
+#include "LED.h"
+
+
+
+
+void LED_Mainfunction_1_20ms(void)
+{
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_SET);
+    HAL_Delay(1000);
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, GPIO_PIN_SET);
+    HAL_Delay(1000);
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2, GPIO_PIN_SET);
+    HAL_Delay(1000);
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2, GPIO_PIN_RESET);
+}
+
+void LED_Mainfunction_2_20ms(void)
+{
+
+}
+
